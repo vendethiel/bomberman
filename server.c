842b8740d9132aba0bb6479ae9a8cb4c600a8619
Theo <salvetti3@live.fr>
2018-03-03 13:52:42 +0100

    Brackets change

--- server.c
+++ server.c
@@ -113,9 +113,7 @@ int	server(int port)
 
 	signal(SIGPIPE, SIG_IGN); /* discard SIGPIPE signals */
 	if (pthread_mutex_init(&server.mutex, NULL) != 0)
-	{
 		ERR_MSG("could not init mutex, errno=%d\n", errno);
-	}
 	set_running(server, 1);
 	server.port = port;
 
